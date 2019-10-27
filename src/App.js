44237ddeb3b01e9d62f98f90e3c0cc12cc18a7c4
Whitny Edwards <wne4e2@gmail.com>
2019-10-27 17:10:51 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import CatList from './CatList.js'
-import { fetchCats } from './actions/catActions'
-
+ 
 class App extends Component {
  
-
-  componentDidMount() {
-    if (this.props.catPics.length === 0) {
-
-      this.props.fetchCats()
-    }
-  }
-
   render() {
+    console.log(this.props.catPics)
     return (
       <div className="App">
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">CatBook</a>
-            </Navbar.Brand>
-          </Navbar.Header>
-        </Navbar>
-        <CatList catPics={this.props.catPics} />
+        <h1>CatBook</h1>
+        {/* missing component */}
       </div>
     );
   }
 }
-
+ 
 const mapStateToProps = state => {
   return {
-    catPics: state.catsReducer.pictures
-    
+    catPics: state.cats,
+    loading: state.loading
   }
 }
  
-export default connect(mapStateToProps, {fetchCats}
-  )(App)
+export default connect(mapStateToProps)(App)
--- src/CatList.js
+++ src/CatList.js
@@ -1,25 +1,3 @@
 // write your CatList component here
 
 
-import React from 'react'
-
-const CatList = (props) => {
-  function list() {
-    return props.catPics.map(catPic => {
-      return (
-        <div className="col-lg-12">
-          <div className="col-lg-6 col-lg-offset-3 well">
-            <img src={catPic.url} className="thumbnail responsive" alt=" " style={{height: '220px', width: '221px', margin: 'auto'}}/>
-          </div>
-        </div>
-      )
-    })
-  }
-  return (
-    <div>
-      {list()}
-    </div>
-  )
-}
-
-export default CatList
